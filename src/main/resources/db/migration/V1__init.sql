613e81a29a9ab2bedfc1a5b7a97f99b9ce831734
bafkit <konyrevpv@mail.ru>
2022-07-23 17:48:15 +0300

    adding category and filter by category

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,27 +1,41 @@
 DROP TABLE IF EXISTS products CASCADE;
-create table products
+create table categories
 (
-    id         bigserial PRIMARY KEY,
-    title      VARCHAR(255),
-    cost       numeric(8, 2),
-    created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp
+    id    bigserial PRIMARY KEY,
+    title VARCHAR(255)
 );
 
-INSERT INTO products (title, cost)
-VALUES ('bread', 250.00),
-       ('Milk', 80.00),
-       ('Orange', 120.00),
-       ('Butter', 50.00),
-       ('Salt', 5.00),
-       ('Сarrot', 30.00),
-       ('Grape', 75.00),
-       ('Sauce', 55.00),
-       ('Cabbage', 33.00),
-       ('Strawberry', 135.00),
-       ('Wine', 150.00),
-       ('Juice', 40.00),
-       ('Banana', 60.00);
+INSERT INTO categories (title)
+VALUES ('Grocery'),
+       ('Beverages'),
+       ('fruits'),
+       ('Vegetables');
+
+
+create table products
+(
+    id            bigserial PRIMARY KEY,
+    title         VARCHAR(255),
+    cost          numeric(8, 2),
+    category_id   bigserial not null references categories (id),
+    created_at    timestamp default current_timestamp,
+    updated_at    timestamp default current_timestamp
+);
+
+INSERT INTO products (title, cost, category_id)
+VALUES ('bread', 250.00, 1),
+       ('Milk', 80.00, 2),
+       ('Orange', 120.00, 3),
+       ('Butter', 50.00, 1),
+       ('Salt', 5.00, 1),
+       ('Сarrot', 30.00, 4),
+       ('Grape', 75.00, 3),
+       ('Sauce', 55.00, 2),
+       ('Cabbage', 33.00, 4),
+       ('Strawberry', 135.00, 3),
+       ('Wine', 150.00, 2),
+       ('Juice', 40.00, 2),
+       ('Banana', 60.00, 3);
 
 create table users
 (
@@ -85,8 +99,8 @@ create table order_items
     updated_at        timestamp default current_timestamp
 );
 
-insert into orders (user_id, total_price, address, phone) values
-(1, 200, 'address', '12345');
+insert into orders (user_id, total_price, address, phone)
+values (1, 200, 'address', '12345');
 
-insert into order_items (product_id, order_id, quantity, price_per_product, price) values
-(1, 1, 2, 100, 200);
+insert into order_items (product_id, order_id, quantity, price_per_product, price)
+values (1, 1, 2, 100, 200);
