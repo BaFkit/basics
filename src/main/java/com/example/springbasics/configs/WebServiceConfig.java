8ff21debd31839fe780d2dc1cb18b40808da1afe
bafkit <konyrevpv@mail.ru>
2022-07-24 23:06:29 +0300

    Work on adding SOAP

---
+++ src/main/java/com/example/springbasics/configs/WebServiceConfig.java
@@ -0,0 +1,17 @@
+package com.example.springbasics.configs;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+
+public class WebServiceConfig extends WsConfigurerAdapter {
+    @Bean
+    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean(servlet, "/ws/*");
+    }
+}
---
+++ src/main/java/com/example/springbasics/endpoints/ProductEndpoint.java
@@ -0,0 +1,11 @@
+package com.example.springbasics.endpoints;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+
+@Endpoint
+@RequiredArgsConstructor
+public class ProductEndpoint {
+
+
+}
