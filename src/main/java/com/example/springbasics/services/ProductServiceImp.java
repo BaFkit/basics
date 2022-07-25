308ac1fd7ee05d50f17106ee7760226d9eb8e58a
bafkit <konyrevpv@mail.ru>
2022-07-25 19:37:32 +0300

    Work on adding SOAP

--- src/main/java/com/example/springbasics/services/ProductServiceImp.java
+++ src/main/java/com/example/springbasics/services/ProductServiceImp.java
@@ -13,6 +13,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,6 +21,10 @@ import java.util.Optional;
 public class ProductServiceImp implements ProductService {
 
     private final ProductRepository productRepository;
+
+    public List<Product> getAll() {
+        return productRepository.findAll();
+    }
 
     @Override
     @Transactional(readOnly = true)
