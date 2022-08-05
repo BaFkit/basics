c285b414cc1303205237eda668086dea4f36f31a
bafkit <konyrevpv@mail.ru>
2022-08-05 18:52:10 +0300

    Separation into microservices

--- cart-service/src/main/java/com/example/springbasics/cart/configs/RedisConfig.java
+++ cart-service/src/main/java/com/example/springbasics/cart/configs/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.example.springbasics.cart.config;
+package com.example.springbasics.cart.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ cart-service/src/main/java/com/example/springbasics/cart/converters/CartConverter.java
@@ -0,0 +1,20 @@
+package com.example.springbasics.cart.converters;
+
+import com.example.springbasics.api.carts.CartDto;
+import com.example.springbasics.api.carts.CartItemDto;
+import models.Cart;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class CartConverter {
+    public CartDto modelToDto(Cart cart) {
+        List<CartItemDto> cartItemDtos = cart.getItems().stream().map(it ->
+                new CartItemDto(it.getProductId(), it.getProductTitle(), it.getQuantity(), it.getPricePerProduct(), it.getPrice())
+        ).collect(Collectors.toList());
+        CartDto cartDto = new CartDto(cartItemDtos, cart.getTotalPrice());
+        return cartDto;
+    }
+}
